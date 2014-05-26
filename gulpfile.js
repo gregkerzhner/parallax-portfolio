76873d44057ec43f3992fbeff2dcbe644aab0064
Greg Kerzhner <greg@Gregs-MacBook-Air.local>
2014-05-26 15:09:52 -0600

    GK: gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,8 @@ es           = require('event-stream'),
 sass         = require('gulp-sass'),
 autoprefixer = require('gulp-autoprefixer'),
 jshint       = require('gulp-jshint'),
-coffee       = require('gulp-coffee'),
+concat = require('gulp-concat'),
+runSequence = require('run-sequence'),
 clean        = require('gulp-clean'),
 connect      = require('gulp-connect'),
 browserify   = require('gulp-browserify'),
@@ -17,99 +18,44 @@ usemin       = require('gulp-usemin'),
 imagemin     = require('gulp-imagemin'),
 rename = require('gulp-rename');
 
-// Connect Task
-gulp.task('connect', connect.server({
-  root: ['./app'],
-  port: 1337,
-  livereload: true
-}));
-
-// Html reload
-gulp.task('html', function () {
-  return gulp.src('./app/**/*.html')
-    .pipe(connect.reload());
+gulp.task('scripts', function() {
+  return gulp.src([
+  	'app/vendors/*/*.min.js', 
+  	'app/vendors/jquery/dist/jquery.js',
+  	'app/vendors/bootstrap/dist/js/bootstrap.js', 
+  	'app/src/*.js'])
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('./public/javascripts'))
+    //.pipe(rename('all.min.js'))
+    //.pipe(uglify())
+    //.pipe(gulp.dest('dist'));
 });
 
-// sass compiler task
-gulp.task('sass', function () {
-  return gulp.src('./app/styles/**/*.scss')
-    .pipe(sass({
-      onError: function (error) {
-        gutil.log(gutil.colors.red(error));
-        gutil.beep();
-      },
-      onSuccess: function () {
-        gutil.log(gutil.colors.green('Sass styles compiled successfully.'));
-      }
-    }))
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(gulp.dest('./app/styles/'))
-    .pipe(connect.reload());
+gulp.task('styles-compile', function () {
+  console.log("sassing")
+  return gulp.src(['./app/vendors/bootstrap/dist/css/bootstrap.min.css','./app/styles/app.scss'])
+    .pipe(sass())
+    .pipe(gulp.dest('./styles-compile'))
 });
 
-// Minify images
-gulp.task('imagemin', function () {
-  return es.concat(
-    gulp.src('./app/images/**/*.png')
-      .pipe(imagemin())
-      .pipe(gulp.dest('/dest/img')),
-    gulp.src('./app/images/**/*.jpg')
-      .pipe(imagemin())
-      .pipe(gulp.dest('/dest/img')),
-    gulp.src('./app/images/**/*.gif')
-      .pipe(imagemin())
-      .pipe(gulp.dest('/dest/img'))
-  );
+gulp.task('styles-concat', function () {
+  console.log("sassing")
+  return gulp.src('styles-compile/*.css')
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest('./public/stylesheets'))
 });
 
-// CoffeeScript compiler task
-gulp.task('coffee', function () {
-  return gulp.src('app/scripts/**/*.coffee')
-    .pipe(coffee({bare: true})).on('error', gutil.log)
-    .pipe(gulp.dest('app/scripts'));
+gulp.task('styles', function() {
+    runSequence('styles-concat', 'styles-compile', function() {
+        console.log('Run something else');
+    });
 });
 
-// Script task
-gulp.task('scripts', ['coffee'], function () {
-  return gulp.src('app/scripts/app.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-    .pipe(browserify({
-      insertGlobals: true
-    }))
-    .pipe(rename(function (path) {
-      path.basename = 'bundle';
-    }))
-    .pipe(gulp.dest('app/scripts'))
-    .pipe(connect.reload());
+gulp.task('watch', function() {
+  gulp.watch('app/vendors/*/*.min.js', ['scripts']);
+  gulp.watch('app/src/*.js', ['scripts']);
+  gulp.watch('app/styles/*.*scss', ['styles']);
 });
 
-gulp.task('watch', function () {
-  gulp.watch([ 'app/styles/**/*.scss'], ['sass']);
-  gulp.watch([ 'app/scripts' + '/**/*.js'], ['scripts']);
-  gulp.watch(['./app/**/*.html'], ['html']);
-});
 
-gulp.task('serve', ['connect', 'sass', 'scripts', 'watch']);
-
-gulp.task('clean', function () {
-  gutil.log('Clean task goes here...');
-});
-
-gulp.task('usemin', function () {
-  gulp.src('./app/**/*.html')
-    .pipe(usemin())
-    .pipe(gulp.dest('./dist/'));
-});
-
-gulp.task('clean-build', function () {
-  return gulp.src('dist/', {read: false})
-    .pipe(clean());
-});
-
-gulp.task('build', ['clean-build', 'sass', 'scripts', 'imagemin', 'usemin'], function () {
-});
-
-gulp.task('default', function () {
-  gutil.log('Default task goes here...');
-});
+gulp.task('default', ['scripts', 'watch', 'styles']);
